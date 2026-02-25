39baec1cafad6e25067b4edb60bfebf306c77f45
DESH DEEPAK GAUTAM <deshgautam03@gmail.com>
2026-02-25 23:11:04 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['@tensorflow-models/pose-detection'],
+  outputFileTracingRoot: path.join(__dirname, '../'),
 };
 
 export default nextConfig;
