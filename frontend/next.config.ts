c3ca298d808487fca8f18b476e50eab5c2f1ec03
DESH DEEPAK GAUTAM <deshgautam03@gmail.com>
2026-02-25 23:23:39 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   transpilePackages: ['@tensorflow-models/pose-detection'],
   outputFileTracingRoot: path.join(__dirname, '../'),
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@tensorflow/**',
+      'node_modules/@mediapipe/**',
+      'node_modules/sharp/**',
+      'node_modules/canvas/**',
+    ],
+  },
 };
 
 export default nextConfig;
